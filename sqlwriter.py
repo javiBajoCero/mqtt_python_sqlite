aebd65ef3fcff466fee4f404db16fda41ad6fde1
javi <javimusama@gmail.com>
2020-01-16 16:44:51 +0100

    Update sqlwriter.py

--- sqlwriter.py
+++ sqlwriter.py
@@ -8,7 +8,7 @@ tableDb = "batterydataA"
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
+    createTableDb()
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe(all_topics)
@@ -29,6 +29,14 @@ def writeToDb(datatoDb):
     print ("Writing to db...")
     c.execute("INSERT INTO"+tableDb+"VALUES ("+ datatoDb +")")
     conn.commit()
+
+def createTableDb()
+    print ("creating table E")
+    connection = sqlite3.connect(dbFile)
+    cursor = connection.cursor()
+    sql ='''CREATE TABLE E (FIRST_NAME CHAR(20) NOT NULL )'''
+    cursor.execute(sql)
+    connection.commit()
     
 client = mqtt.Client()
 client.on_connect = on_connect
